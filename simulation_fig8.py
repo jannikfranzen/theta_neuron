9b946cbe8c5bc02f360da5a37e161a34adab5226
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2022-07-22 13:34:28 +0200

    Update simulation_fig8.py

--- simulation_fig8.py
+++ simulation_fig8.py
@@ -1,20 +1,20 @@
+from tokenize import PlainToken
 from simulation import theta_torch
 import numpy as np
 import time
 import pickle
 from math import pi,sqrt
+import matplotlib.pyplot as plt
 
 T = 20
-dt = 0.002
-sigma = 1
-n_ens = 1000000
-n_relax = int()
-t_vec = np.zeros(int(T/dt))
+dt = 0.01
+n_ens = 10000000
+t_vec = np.arange(0,T,dt)
 noise_ens = np.zeros(n_ens)
-phase_ens = np.linspace(0,2*pi,n_ens)
+phase_ens = np.zeros(n_ens)#np.linspace(0,2*pi,n_ens)
 
 # model parameter
-mu = 1
+mu = 0.5
 sigma = 1
 tau = 1
 epsilon = 0.1
@@ -22,12 +22,12 @@ omega = 2
 
 signal = epsilon*np.cos(omega*t_vec)
 
-firing_rate,_,noise_ens,phase_ens = theta_torch(signal,dt,mu,sigma,tau,noise_ens,phase_ens) # reuse phase and noise dist for adjascent points
-
-
-
+firing_rate,_,noise_ens,phase_ens = theta_torch(signal,dt,mu,sigma,tau,noise_ens,phase_ens) 
 
 name = 'theta_sim_fig8'
 f = open(name + '.pckl', 'wb')
-pickle.dump([dt,T,n_ens,tau,sigma,mu,firing_rate], f)
+pickle.dump([dt,T,n_ens,tau,sigma,mu,epsilon,omega,firing_rate], f)
 f.close()
+
+plt.plot(firing_rate)
+plt.show()
